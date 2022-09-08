9fe1daf7b3e39757dc05aa23f5a19a6bcc5ead66
Atu175 <59740296+Atu175@users.noreply.github.com>
2022-09-08 15:28:07 -0400

    Added classes

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-PERMISSION = {
-    
-}
+PERMISSION = ["public", "secret", "top", "majestys"]
+
+
+class Documents:
+    def __init__(self, permission, filename):
+        pass
+
+
+class Person:
+    def __init__(self, permission):
+        pass
+
+    def createFile(self, filename):
+        pass
+
+    def readFile(self, document):
+        pass
+
+    def writeFile(self, document, text):
+        pass
+
