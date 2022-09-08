b82aba9ad4460f59bb896fa635fd272f51b237b7
Atu175 <59740296+Atu175@users.noreply.github.com>
2022-09-08 16:05:48 -0400

    Need correction

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 PERMISSION = ["public", "secret", "top", "majestys"]
 
 
@@ -8,14 +9,36 @@ class Documents:
 
 class Person:
     def __init__(self, permission):
-        pass
+        self.permission = permission
 
     def createFile(self, filename):
-        pass
+        if ".txt" not in filename:
+            filename = filename + ".txt"
+        path = "files"
+        file_directory = os.listdir(path)
+        if filename not in file_directory:
+            f = open("file/" + filename, "w")
+            f.write(str(self.permission) + "\n")
+            f.close()
+            print(f"{filename} created succesfully!")
+        else:
+            print(f"Sorry , but the file already exist")
 
     def readFile(self, document):
-        pass
+        with open(document.filename, "r") as f:
+            if PERMISSION.index(self.permission) >= PERMISSION.index(document.permission):
+                f.readlines()
+            else:
+                print("Permission denied to read from {} to {}".format(self.permission, document.permission))
 
     def writeFile(self, document, text):
-        pass
+        with open(document.filename, "a") as f:
+            if PERMISSION.index(self.permission) <= PERMISSION.index(document.permission):
+                f.writelines("\n")
+                f.writelines(text)
+            else:
+                print("Permission denied to write from {} to {}".format(self.permission, document.permission))
+if __name__ == "__main__":
+    pass
+
 
